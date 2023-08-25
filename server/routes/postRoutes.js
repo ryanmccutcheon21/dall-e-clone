aa2f27a6ae6baeca3fcf81f6b1a5d5ce878bf591
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-08-24 20:48:56 -0500

    connect to cloudinary and create get and post request functionality

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,42 @@ dotenv.config()
 
 const router = express.Router()
 
+// Configure Cloudinary to use API from .env file
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+// GET all posts
+router.route('/').get(async (req, res) => {
+    try {
+        const posts = await Post.find({})
+
+        res.status(200).json({ success: true, data: posts })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+
+})
+// CREATE a post
+router.route('/').post(async (req, res) => {
+    try {
+        // get data sent from frontend
+        const { name, prompt, photo } = req.body
+        // upload photo url to cloudinary
+        const photoUrl = await cloudinary.uploader.upload(photo)
+
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo: photoUrl.url,
+        })
+
+        res.status(201).json({ success: true, data: newPost })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+})
+
 export default router
